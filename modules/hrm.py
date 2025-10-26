0b624454b5e3cc62991f1bbd15014ee3256190d2
Jekoth <80098768+Jekoth@users.noreply.github.com>
2025-10-26 19:16:30 +0200

    test

--- modules/hrm.py
+++ modules/hrm.py
@@ -8,34 +8,32 @@ threshold_min = 500
 time_mills = 5000
 beat = 0
 start_time = time.time()
+prev_value = threshold_min
 
 def getHRM():
         Powered.on()
         time.sleep(0.5)
         Powered.off()
         time.sleep(0.5)
-        
         return HR_Raw.read_u16()
 
-def beat_Count(param1):
-    
-    return param1 - start_time
-
-
 def convert_HR():
+    global beat, start_time, prev_value
     while True:
         raw_Data = getHRM()
         value = raw_Data >> 6
         present = time.time()
-        
-        if beat_Count(present) > time_mills:
-            bpm = beat * 12
+
+        if (prev_value < threshold_min) and (value > threshold_max):
+            beat += 1
+        prev_value = value
+
+        if (present - start_time) * 1000 > time_mills:
+            bpm = int(beat * (60000 / time_mills))
             print(bpm)
             beat = 0
             start_time = present
             time.sleep(0.5)
-        
-
         
 if __name__ == "__main__":
     convert_HR()
