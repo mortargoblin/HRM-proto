ca6d73a56f9de8cbaf96da687a71e2a483a23c7f
4KIL0 <123417481+4KIL0@users.noreply.github.com>
2025-10-26 18:54:01 +0200

    HRM Function in process

--- modules/hrm.py
+++ modules/hrm.py
@@ -3,17 +3,40 @@ import time
 
 HR_Raw = ADC(Pin(27, Pin.IN))
 Powered = Pin("LED", Pin.OUT)
-HR_Real = 0
+threshold_max = 600
+threshold_min = 500
+time_mills = 5000
+beat = 0
+start_time = time.time()
 
 def getHRM():
-    while True:
         Powered.on()
         time.sleep(0.5)
         Powered.off()
-        value = HR_Raw.read_u16()
-        HR_Real = HR_Raw.read_u16() / 4096 * 3.3
         time.sleep(0.5)
-        print(f"HR: {value}")
+        
+        return HR_Raw.read_u16()
 
-getHRM()
+def beat_Count(param1):
+    
+    return param1 - start_time
+
+
+def convert_HR():
+    while True:
+        raw_Data = getHRM()
+        value = raw_Data >> 6
+        present = time.time()
+        
+        if beat_Count(present) > time_mills:
+            bpm = beat * 12
+            print(bpm)
+            beat = 0
+            start_time = present
+            time.sleep(0.5)
+        
+
+        
+if __name__ == "__main__":
+    convert_HR()
 
