c167844a422e1e69963030304440f77a4aa0ffae
mortargoblin <mortargoblin@github.com>
2025-11-04 11:40:29 +0200

    menu state change

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class MenuState:
     OPTION_2: int = 1
     OPTION_3: int = 2
 
+NUM_MENU_STATES: int = 3
+current_state = MenuState.HR_DISPLAY
+
 def main():
 
     ### MAIN LOOP ###
@@ -17,6 +20,10 @@ def main():
 
         # input handling
         print(Rotatory.Encoder.value(), "VALUE")
+
+        # next menu option
+        if Rotatory.Encoder.value() == 1:
+            current_state = (current_state + 1) % NUM_MENU_STATES
 
 
 if __name__=="__main__":
